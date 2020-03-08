5b276efde071a65297430a2d8a52d42effe877ca
nagarajumadamshetti <nagarajumadamshetti@gmail.com>
2020-03-08 13:03:58 +0530

    assignment4 redux

--- burgerapp/src/components/Burger/OrderSummary/OrderSummary.js
+++ burgerapp/src/components/Burger/OrderSummary/OrderSummary.js
@@ -21,7 +21,7 @@ class OrderSummary extends Component {
         return (
             <Aux>
                 <h3>Your Order</h3>
-                <p>A delicious burger with the following ingredients:</p>
+                <p>ingredients:</p>
                 <ul>
                     {ingredientSummary}
                 </ul>
